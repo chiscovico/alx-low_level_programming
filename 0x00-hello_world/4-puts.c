e9258993be47135420a0a30367105ad41535fec9
chiscovico <chinonsookoroafor1@gmail.com>
2023-09-08 03:29:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - this is the main put function
- * * Return: 0
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
